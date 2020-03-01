3d455dd061eb907bc28e91d1891b8735b31f50bc
Kevin Delord <delord.kevin@gmail.com>
2020-03-01 22:13:00 +0100

    integrate slideshow with flickity

---
+++ js/custom/slideshow.js
@@ -0,0 +1,13 @@
+/*!
+ * Custom Javascript for the Image Slideshow
+ */
+
+/* Open */
+function openSlideshow() {
+  document.getElementById("slideshow").style.height = "100%";
+}
+
+/* Close */
+function closeSlideshow() {
+  document.getElementById("slideshow").style.height = "0%";
+}
