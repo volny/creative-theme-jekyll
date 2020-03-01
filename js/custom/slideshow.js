90e95d28583d17cac4e68346ea8162ab3cb69478
Kevin Delord <delord.kevin@gmail.com>
2020-03-01 22:18:15 +0100

    Slideshow: on ESC press, close slideshow

--- js/custom/slideshow.js
+++ js/custom/slideshow.js
@@ -4,7 +4,15 @@
 
 /* Open */
 function openSlideshow() {
-  document.getElementById("slideshow").style.height = "100%";
+	document.getElementById("slideshow").style.height = "100%";
+
+	// keyboard event
+	document.addEventListener( 'keydown', function( ev ) {
+		var keyCode = ev.keyCode || ev.which;
+		if (keyCode == 0 || keyCode == 27 || keyCode == 32) { // esc, space (chrome+firefox)
+			closeSlideshow();
+		}
+	});
 }
 
 /* Close */
