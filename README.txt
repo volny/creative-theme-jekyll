c821b323f130b8341da595493021d533b896446d
aelder202 <taylor.elder328@gmail.com>
2021-10-17 13:07:22 -0400

    new site

--- README.txt
+++ README.txt
@@ -1,10 +1,12 @@
-Miniport by HTML5 UP
+Striped by HTML5 UP
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-A super simple responsive portfolio template. It's only a single page because hey, sometimes
-you don't need a half dozen pages to make your point.
+And here's a new one: Striped, which features a clean, minimalistic design, styling for
+all basic page elements (including blockquotes, tables and lists), a repositionable
+sidebar (left or right), and HTML5/CSS3 code designed for quick and easy customization
+(see code comments for details).
 
 Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
 you can use for pretty much whatever.
@@ -16,8 +18,6 @@ Feedback, bug reports, and comments are not only welcome, but strongly encourage
 AJ
 aj@lkn.io | @ajlkn
 
-PS: Not sure how to get that contact form working? Give formspree.io a try (it's awesome).
-
 
 Credits:
 
@@ -25,8 +25,11 @@ Credits:
 		Unsplash (unsplash.com)
 
 	Icons:
-		Font Awesome (fontawesome.io)
+		Font Awesome (fortawesome.github.com/Font-Awesome)
 
 	Other:
 		jQuery (jquery.com)
-		Responsive Tools (github.com/ajlkn/responsive-tools)
+		html5shiv.js (@afarkas @jdalton @jon_neal @rem)
+		CSS3 Pie (css3pie.com)
+		Respond.js (j.mp/respondjs)
+		Skel (skel.io)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Miniport by HTML5 UP
+	Striped by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -8,14 +8,15 @@
 
 	var	$window = $(window),
 		$body = $('body'),
-		$nav = $('#nav');
+		$document = $(document);
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
+			desktop:   [ '737px',   null     ],
+			wide:      [ '1201px',  null     ],
+			narrow:    [ '737px',   '1200px' ],
+			narrower:  [ '737px',   '1000px' ],
+			mobile:    [ null,      '736px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -25,10 +26,45 @@
 			}, 100);
 		});
 
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
+	// Nav.
+
+		// Height hack.
+		/*
+			var $sc = $('#sidebar, #content'), tid;
+
+			$window
+				.on('resize', function() {
+					window.clearTimeout(tid);
+					tid = window.setTimeout(function() {
+						$sc.css('min-height', $document.height());
+					}, 100);
+				})
+				.on('load', function() {
+					$window.trigger('resize');
+				})
+				.trigger('resize');
+		*/
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#sidebar" class="toggle"></a>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Sidebar
+			$('#sidebar')
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'sidebar-visible'
+				});
 
 })(jQuery);
