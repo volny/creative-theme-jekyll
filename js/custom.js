545ace2cd32c6a6e8b436a242bad543a1fee9af8
Matt Regan <maishou@gmail.com>
2023-01-10 07:35:26 +0800

    Fixes to pages and tidy

--- js/custom.js
+++ js/custom.js
@@ -3,12 +3,11 @@ new Vue({
   delimiters : ['[[',']]'],
   data() {
     return {
-      message: "🐵 Hello World 🔮",
-      timestamp: `Timestamp ${new Date().toLocaleString()}`,
       bannerHeight: 0,
       navHeight: 0,
       navFill: false,
-      navActive: false
+      navActive: false,
+      teamActive: false
     }
   },
   mounted() {
@@ -23,6 +22,38 @@ new Vue({
   methods: {
     clickNav() {
       this.navActive = !this.navActive;
+    },
+    closeNav() {
+      this.navActive = false;
+    },
+    closeProfile() {
+      const el = document.getElementsByClassName('person');
+
+      Object.values(el).map(item => {
+        item.classList.remove("profile-active");
+        console.log(item);
+      });
+    },
+    showProfile(person) {
+      const myEl = this.$refs[person];
+      const el = document.getElementsByClassName('person');
+
+      Object.values(el).map(item => {
+        item.classList.remove("profile-active");
+        console.log(item);
+      });
+
+      this.$smoothScroll({
+        scrollTo: myEl,
+        offset: -100,
+        duration: 400,
+      });
+
+      setTimeout(() => {
+        this.teamActive = true;
+        this.$refs[person].classList.value = this.$refs[person].classList.value + ' profile-active';
+      }, 350);
+      
     },
     onScroll(e) {
       this.windowTop = window.top.scrollY
