12316f84e106e734c99f4b55255a3d0e789b3afc
Matt Regan <maishou@gmail.com>
2023-02-13 07:06:53 +0800

    Final updates and tweaks

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,10 @@ new Vue({
       navHeight: 0,
       navFill: false,
       navActive: false,
-      teamActive: false
+      teamActive: false,
+      tags: [],
+      selected: "",
+      articles: []
     }
   },
   mounted() {
@@ -15,11 +18,48 @@ new Vue({
     this.bannerHeight = this.$refs.bannerBlock.clientHeight - this.navHeight;
     
     window.addEventListener("scroll", this.onScroll);
+
+    for (let [key, value] of Object.entries(this.$refs)) {
+      if (/^n-/.test(key)) {
+        this.tags.push(key.slice(2));    
+      }
+    }
+
+    this.selected = this.tags[0];
+
+    
+    
+    // console.log(this.articles);
   },
   beforeDestroy() {
     window.removeEventListener("scroll", this.onScroll)
   },
   methods: {
+    filterNews(e) {
+      this.selected = e.target.value;
+
+      this.articles = Object.keys(this.$refs).filter((ref) => {
+        if(ref.includes('article')){
+          return true
+        }
+        return false
+      }).map((val) => {
+        
+        this.$refs[val].classList.remove('active');
+        // let list = this.$refs[val].classList; 
+        if ( this.$refs[val].classList.contains(this.selected)) {
+          this.$refs[val].classList.value = this.$refs[val].classList.value + ' active'; 
+        }
+        // console.log(list);
+        // console.log(list.indexOf('news'));
+        // // if ( this.$refs[val].classList.includes(this.selected)) {
+        //   // console.log(list);
+        //   console.log(list.indexOf('news'));
+        // // }
+        // return this.$refs[val];
+      })
+
+    },
     clickNav() {
       this.navActive = !this.navActive;
     },
